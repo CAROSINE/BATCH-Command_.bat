75a031876943025cf8948f4338de670a1073c86c
CAROSINE <moshiur287rahman@gmail.com>
2026-07-08 11:07:23 +0600

    basic commands

---
+++ basic_batch.bat
@@ -0,0 +1,9 @@
+:: Basic batch file to demonstrate simple commands
+@echo off REM show batch commands
+echo      REM print kore
+pause     REM wait for user input
+cls       REM clear screen
+cd ..\    REM change directory to parent folder
+dir       REM list files and directories
+if        REM for conditional statement
+for      REM to run loop
