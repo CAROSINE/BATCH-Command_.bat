68738377990bd56e091879e6705aec24d0dea447
CAROSINE <moshiur287rahman@gmail.com>
2026-07-13 23:12:15 +0600

    System

---
+++ login-system.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0a
+title Login System
+
+:login
+cls
+echo ==========================
+echo       LOGIN SYSTEM
+echo ==========================
+set /p user=Username:
+set /p pass=Password:
