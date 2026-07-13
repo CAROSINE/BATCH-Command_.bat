f8b6e29049931dfe0e7c9c84848a3e599e9641e4
CAROSINE <moshiur287rahman@gmail.com>
2026-07-13 23:15:26 +0600

    System

--- login-system.bat
+++ login-system.bat
@@ -9,3 +9,10 @@ echo       LOGIN SYSTEM
 echo ==========================
 set /p user=Username:
 set /p pass=Password:
+
+if %user%=="admin" if %pass%=="1234" got success
+echo.
+echo Invalid credentials!
+pause
+goto login
+
