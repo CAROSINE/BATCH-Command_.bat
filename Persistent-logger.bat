f9abed6bed1dd02245a15894d03f5cbfa2438778
CAROSINE <moshiur287rahman@gmail.com>
2026-07-21 21:18:23 +0600

    Persistent logger

--- Persistent-logger.bat
+++ Persistent-logger.bat
@@ -14,3 +14,10 @@ echo 3. Delete hidden log
 echo 4. Add to startup
 echo 5. Exit
 set /p opt=Choice:
+
+if "%opt%"=="1" goto writelog
+if "%opt%"=="2" goto viewlog
+if "%opt%"=="3" goto dellog
+if "%opt%"=="4" goto addstartup
+if "%opt%"=="5" goto end
+goto log
