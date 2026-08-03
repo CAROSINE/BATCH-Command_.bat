a2992dcf5fe899ce25fe163f36376d1f303625a2
CAROSINE <moshiur287rahman@gmail.com>
2026-08-03 21:45:39 +0600

    using powershell-base 64 decryption

--- file-encryption.bat
+++ file-encryption.bat
@@ -1,8 +1,10 @@
 @echo off
-:: encript a file using base64 encoding
+
+:: encrypt a file using base64 encoding
 echo Secret password: 1234 > plain.txt
 powershell -Command "[Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes((Get-Content plain.txt -Raw)))" > encrypted.txt
 echo Encrypted data saved in encrypted.txt
 echo.
+
 type encrypted.txt
-pause
+pause 
